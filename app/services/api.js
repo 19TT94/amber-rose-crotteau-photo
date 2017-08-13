34ce27a5dbb82da1820ceeb4779a919a308db35f
Taylor Tobin <19tt94@gmail.com>
2017-08-12 23:25:21 -0700

    added featured menu

--- app/services/api.js
+++ app/services/api.js
@@ -2,22 +2,27 @@ import Ember from 'ember';
 
 const home = [
     {
+        'button':'Wild',
         'color':'#FFCEE5',
         'url':'/images/cat3.jpg'
     },
     {
+        'button':'Humans',
         'color':'#FCF6BD',
         'url':'/images/human1.jpg'
     },
     {
+        'button':'Land',
         'color':'#D0F4DE',
-        'url':'/images/land2.jpg'
+        'url':'/images/land2.JPG'
     },
     {
+        'button':'Sea',
         'color':'#A9DEF9',
         'url':'/images/ocean1.jpg'
     },
     {
+        'button':'Surf',
         'color':'#E4C1F9',
         'url':'/images/surf1.jpg'
     }
