1b7ab1e726154509f3f24c564e72e32f8640e1bc
Taylor Tobin <19tt94@gmail.com>
2017-09-05 09:15:31 -0700

    first draft ready to publish

--- app/services/api.js
+++ app/services/api.js
@@ -14,22 +14,22 @@ const home = [
         'color':'#424B54',
         'url':'/images/human1.jpg',
         'slide':'/images/human1.jpg',
-        'slide1':'/images/human.jpg',
-        'slide2':'/images/human2.jpg'
+        'slide1':'/images/human.JPG',
+        'slide2':'/images/human3.jpg'
     },
     {
         'button':'land',
         'color':'#7C90A0',
-        'url':'/images/land.jpg',
-        'slide':'/images/land.jpg',
-        'slide1':'/images/land1.jpg',
-        'slide2':'/images/land2.jpg'
+        'url':'/images/land5.JPG',
+        'slide':'/images/land5.JPG',
+        'slide1':'/images/ocean1.jpg',
+        'slide2':'/images/land2.JPG'
     },
     {
         'button':'sea',
         'color':'#C1D7DD',
-        'url':'/images/surf.jpg',
-        'slide':'/images/surf.jpg',
+        'url':'/images/surf5.JPG',
+        'slide':'/images/surf5.JPG',
         'slide1':'/images/surf1.jpg',
         'slide2':'/images/surf2.jpg'
     },
@@ -38,7 +38,7 @@ const home = [
         'color':'#596869',
         'url':'/images/cat3.jpg',
         'slide':'/images/cat3.jpg',
-        'slide1':'/images/cat.jpg',
+        'slide1':'/images/cat2.jpg',
         'slide2':'/images/cat1.jpg'
     }
 ];
